50b6f3efe928d5765fb933a198c4fd7023a3a4fe
Pablo <pabloag43@educastur.es>
2023-07-12 10:21:23 +0200

    Mostramos los errores en la pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 3000,
+  inStorage: 246,
+
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicPageComponent implements OnInit {
 
-  constructor() { }
+  // public myForm: FormGroup = new FormGroup({
+  //    name: new FormControl('', [], []),
+  //    price: new FormControl(0),
+  //    inStorage: new FormControl(0),
+  // })
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(1)]],
+    inStorage: [0, [Validators.required, Validators.min(1)]],
+
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm.reset(rtx5090)
   }
 
+  // metodo que usaremos para guardar los elementos del formulario
+  onSave(): void {
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value)
+
+    // restablecemos el valor del formulario a sus valores por defecto
+    this.myForm.reset({ price: 0, inStorage: 0 })
+  }
 }
