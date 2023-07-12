f18619596913dd089ece59d2b8059b3cfcb9dfdd
Pablo <pabloag43@educastur.es>
2023-07-12 09:43:24 +0200

    Añadimos la sideNav

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { title } from 'process';
+
+interface MenuItem {
+  title: string,
+  route: string,
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent {
+
+  public reactiveMenu: MenuItem[] = [
+    { title: 'Básicos', route: './reactive/basic' },
+    { title: 'Dinámicos', route: './reactive/dynamic' },
+    { title: 'Switches', route: './reactive/switches' },
+
+  ];
+
+  public authMenu: MenuItem[] = [
+    { title: 'Registro', route: './auth' },
+  ];
+}
