f84538827fe0b75decff476180e8699fa7d990a1
Pablo <pabloag43@educastur.es>
2023-07-12 09:25:10 +0200

    Añadimos los html a las paginas

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+export class BasicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
