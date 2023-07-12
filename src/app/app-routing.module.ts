058b9f7f5c532ebd69ab96602c380c745e7ba812
Pablo <pabloag43@educastur.es>
2023-07-12 09:34:07 +0200

    Agregamos las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then(p => p.ReactiveModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(p => p.AuthModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
